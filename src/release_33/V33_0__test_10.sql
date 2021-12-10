821dd72e7942495174edc522cf7f4a6df2baeb08
teamcity <teamcity@mmbank.ru>
2021-12-10 16:01:12 +0000

    version 33

---
+++ src/release_33/V33_0__test_10.sql
@@ -0,0 +1,8 @@
+create table t10 (f1 number);
+create table t10_1 (f1 number);
+create table t10_2 (f1 number);
+create table t10_3 (f1 number);
+create table t10_4 (f1 number);
+create table t10_5 (f1 number);
+create table t10_6 (f1 number);
+/
